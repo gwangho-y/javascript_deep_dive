21d4f4354e29bb6ec4e2007f471f8e1b88324da2
yi-gwangho <dlrhkdgh21@gmail.com>
2023-06-07 07:47:45 +0900

    7.1.2 단항 산술 연산자

---
+++ 07 연산자/07-13.js
@@ -0,0 +1,9 @@
+
+// 비교 연산자
+// 동등 비교, 결과 예측 어렵다
+console.log('0' == '') // false
+console.log(0 =='') // true
+console.log(0 == '0') // true
+console.log(false == '0')  // true
+console.log(false == null) // false
+console.log(false == undefined) // false
