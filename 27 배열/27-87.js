3d67eaef121fcb8413c654e7b753dde3b0eb9700
gwangho-y <yigwangho21@gmail.com>
2023-06-27 09:06:17 +0900

    27 배열

---
+++ 27 배열/27-87.js
@@ -0,0 +1,8 @@
+const fruits = ['Banana', 'Orange', 'Apple'];
+fruits.sort()
+console.log(fruits) // [ 'Apple', 'Banana', 'Orange' ]
+
+const numbers = [5,3,1,4,2]
+numbers.sort()
+console.log(numbers) // [ 1, 2, 3, 4, 5 ]
+
